1f731addb1980db79cafd99e8b002ef2c3e22a2f
odrya-keri-mako <95348097+odrya-keri-mako@users.noreply.github.com>
2026-03-16 16:50:43 +0100

    Add unique extensions array and log to console

--- extensions.js
+++ extensions.js
@@ -1,1 +1,17 @@
+let extensions = [
+  'xdebug.php-pack',
+  'donjayamanne.python-extension-pack',
+  'ms-vscode.notepadplusplus-keybindings',
+  'pkief.material-icon-theme',
+  'yzhang.markdown-all-in-one',
+  'tomoki1207.pdf',
+  'hossaini.bootstrap-intellisense',
+  'bradlc.vscode-tailwindcss',
+  'xabikos.javascript',
+  'svelte.svelte-vscode',
+  'vue.volar',
+  'jawandarajbir.react-vscode-extension-pack'
+];
 
+let uniqueExtensions = [...new Set(extensions)];
+console.log(uniqueExtensions);
